4d169b382b4707f402fd34a595d4164f8b7b8a75
Zarar Ashraf <zararashraf01@gmail.com>
2022-03-31 10:02:26 +0500

    Added Navbar/Free Brands Icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <React.Fragment>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </React.Fragment>
   );
 }
 
