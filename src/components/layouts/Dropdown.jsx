2b4da2f19a75fea72537d5e501d474bdbf44364b
Zarar Ashraf <zararashraf01@gmail.com>
2022-04-01 16:14:41 +0500

    Working on Navbar

--- src/components/layouts/Dropdown.jsx
+++ src/components/layouts/Dropdown.jsx
@@ -27,7 +27,7 @@ export default function DropDown({text, icon, options}) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="origin-top-right absolute left-0 mt-2 w-56 shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             {options.map((option) => (
                 <Menu.Item>
@@ -35,7 +35,7 @@ export default function DropDown({text, icon, options}) {
                   <a
                     href="#"
                     className={classNames(
-                      active ? 'text-cyan-800' : 'text-gray-700',
+                      active ? 'bg-gray-300 duration-800' : 'text-gray-700',
                       'block px-4 py-2 text-sm'
                     )}
                   >
--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -4,7 +4,15 @@ import { faMailchimp, faYoutube, faYoutubeSquare } from "@fortawesome/free-brand
 
 
 const Navbar = () => {
-  const seoItems = ['Keyword Research Tool', 'Bulk Keyword Data Tool', 'Questions Explorer', 'Backlinks Checker', 'Backlinks List', 'High CPC Keywords', 'SEO Analyzer'];
+  const seoItems = [
+    { 'icon': '', 'text': 'Keyword Research Tool' }, 
+    { 'icon': '', 'text': 'Bulk Keyword Data Tool' }, 
+    { 'icon': '', 'text': 'Questions Explorer' }, 
+    { 'icon': '', 'text': 'Backlinks Checker' }, 
+    { 'icon': '', 'text': 'Backlinks List' }, 
+    { 'icon': '', 'text': 'High CPC Keywords' },
+    { 'icon': '', 'text': 'SEO Analyzer' }
+  ];
   
   const youtubeItems = [
     { 'icon': faYoutubeSquare, 'text': 'YouTube Keyword Tool' },
@@ -16,10 +24,37 @@ const Navbar = () => {
     { 'icon': faParagraph, 'text': 'Paragraph Translator' }
   ];
 
-  const emailItems = ['Email Validation Tool', 'Bulk Email Validation', 'Email TXT Extraction Tool', 'Warmup Schedule Generator', 'Subject Line Tester'];
-  const discoverItems = ['High CPC Keywords', 'Influencers', 'Instagram Hashtags', 'Affiliate Programs'];
-  const webtoolItems = ['Privacy Policy Generator', 'Terms & Conditions Generator', 'Fast Domain Search', 'Text Sharing'];
-  const moreItems = ['Guides', 'Z-educate', 'Z-educate Forums', 'Link URL Tracker', 'Fast Domain Search', 'InQuick Sticks', 'Super Email Sender'];
+  const emailItems = [
+    { 'icon': '', 'text': 'Email Validation Tool' }, 
+    { 'icon': '', 'text': 'Bulk Email Validation' }, 
+    { 'icon': '', 'text': 'Email TXT Extraction Tool' }, 
+    { 'icon': '', 'text': 'Warmup Schedule Generator' }, 
+    { 'icon': '', 'text': 'Subject Line Tester' },
+  ];
+
+  const discoverItems = [
+    { 'icon': '', 'text': 'High CPC Keywords' }, 
+    { 'icon': '', 'text': 'Influencers' }, 
+    { 'icon': '', 'text': 'Instagram Hashtags' }, 
+    { 'icon': '', 'text': 'Affiliate Programs' }, 
+  ];
+
+  const webtoolItems = [
+    { 'icon': '', 'text': 'Privacy Policy Generator' }, 
+    { 'icon': '', 'text': 'Terms & Conditions Generator' }, 
+    { 'icon': '', 'text': 'Fast Domain Search' }, 
+    { 'icon': '', 'text': 'Text Sharing' }, 
+  ];
+
+  const moreItems = [
+    { 'icon': '', 'text': 'Guides' }, 
+    { 'icon': '', 'text': 'Z-educate' }, 
+    { 'icon': '', 'text': 'Z-educate Forums' }, 
+    { 'icon': '', 'text': 'Link URL Tracker' }, 
+    { 'icon': '', 'text': 'Fast Domain Search' }, 
+    { 'icon': '', 'text': 'InQuick Sticks' }, 
+    { 'icon': '', 'text': 'Super Email Sender' }, 
+  ];
 
   return (
     <nav className='bg-white border-gray-200 px-2 sm:px-4 py-10 shadow-lg'>
@@ -30,43 +65,14 @@ const Navbar = () => {
           <span className='self-center text-xl font-semibold whitespace-nowrap text-stone-900'>RokuOne</span>
         </a>
 
-        <div className='flex items-center md:order-2'>
-          <button type='button' className='flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600' id='user-menu-button' aria-expanded='false' data-dropdown-toggle='dropdown'>
-            <span className='sr-only'>Open user menu</span>
-            {/*<img className='w-8 h-8 rounded-full' alt='user photo' />*/}
-          </button>
+        <button data-collapse-toggle='mobile-menu-2' type='button' className='inline-flex duration-500 items-center p-2 ml-1 text-sm text-gray-500 md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600' aria-controls='mobile-menu-2' aria-expanded='false'>
+          <span className='sr-only'>Open main menu</span>
+          <svg className='w-6 h-6' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z' clip-rule='evenodd'></path></svg>
+          <svg className='hidden w-6 h-6' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z' clip-rule='evenodd'></path></svg>
+        </button>
 
-          {/* Dropdown menu */}
-          <div className='hidden z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600' id='dropdown'>
-            <div className='py-3 px-4'>
-              <span className='block text-sm text-gray-900 dark:text-white'>Bonnie Green</span>
-              <span className='block text-sm font-medium text-gray-500 truncate dark:text-gray-400'>name@flowbite.com</span>
-            </div>
-            <ul className='py-1' aria-labelledby='dropdown'>
-              <li>
-                <a href='#' className='block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white'>Dashboard</a>
-              </li>
-              <li>
-                <a href='#' className='block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white'>Settings</a>
-              </li>
-              <li>
-                <a href='#' className='block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white'>Earnings</a>
-              </li>
-              <li>
-                <a href='#' className='block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white'>Sign out</a>
-              </li>
-            </ul>
-          </div>
-
-          <button data-collapse-toggle='mobile-menu-2' type='button' className='inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600' aria-controls='mobile-menu-2' aria-expanded='false'>
-            <span className='sr-only'>Open main menu</span>
-            <svg className='w-6 h-6' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z' clip-rule='evenodd'></path></svg>
-            <svg className='hidden w-6 h-6' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z' clip-rule='evenodd'></path></svg>
-          </button>
-        </div>
-
-        <div className='hidden justify-between items-center w-full md:flex md:w-auto md:order-1' id='mobile-menu-2'>
-          <ul className='flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium'>
+        <div className='hidden mr-0 justify-between items-center w-full md:flex md:w-auto md:order-1' id='mobile-menu-2'>
+          <ul className='flex flex-col mt-4 md:flex-row md:space-x-4 md:mt-0 md:text-sm md:font-medium'>
             <li>
               <DropDown text='SEO' icon={faChartSimple} options={seoItems} />
             </li>
